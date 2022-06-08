57d5df806e2d7a2628c8b427dce3f5c1cf419d63
ahmed-k-ezz <ahmed01009344590>
2022-06-08 17:10:23 +0200

    Installing primeNg

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {InputTextModule} from 'primeng/inputtext';
+import {RadioButtonModule} from 'primeng/radiobutton';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    InputTextModule,
+    RadioButtonModule
+  ],
+  providers: []
+})
+
+export class SharedModule {}
